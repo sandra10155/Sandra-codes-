c9597b917316a7b5beaa6e0ce9614adbb380169d
sandra10155 <sandra.28702@students.kyu.ac.ke>
2025-10-09 08:45:25 +0300

    Add files via upload

---
+++ for loop .c
@@ -0,0 +1,16 @@
+/*
+Name: Sandra Chepchirchir 
+Registration No: PA106/G/28702/25
+Description :Program to display the for loop 
+*/
+
+#include <stdio.h>
+
+int main() {
+int i;
+for(i=100;i>=50;i--)
+{
+printf("%d\n",i);
+}
+return 0;
+}
